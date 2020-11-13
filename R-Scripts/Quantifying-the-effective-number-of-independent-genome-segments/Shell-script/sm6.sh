79b0f665d2c619e71875a26699245896a0aee2db
Medhat68 <medhat.a.abbas@gmail.com>
2020-11-13 17:29:06 +0100

    b

---
+++ R-Scripts/Quantifying-the-effective-number-of-independent-genome-segments/Shell-script/sm6.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -p fat
+#SBATCH -t 48:00:00
+#SBATCH -J simpleM_6
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH --mem=250G
+#SBATCH -o simpleM_6.out
+#SBATCH -e simpleM_6.err
+
+
+
+module load intel/mkl/64/2017/2.174
+
+
+module load openmpi/intel/64/1.10.7
+
+
+#module load conda/4.3.30
+#source activate party 
+Rscript  simpleM_6.r
